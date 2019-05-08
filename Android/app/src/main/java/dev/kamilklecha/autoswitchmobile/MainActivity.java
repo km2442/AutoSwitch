2085678f183c04f7bae833a69c7d6019296c9b2f
Unknown <urlikodd@gmail.com>
2019-05-09 00:10:31 +0200

    Moved Sockets to another class

--- Android/app/src/main/java/dev/kamilklecha/autoswitchmobile/MainActivity.java
+++ Android/app/src/main/java/dev/kamilklecha/autoswitchmobile/MainActivity.java
@@ -1,8 +1,8 @@
 package dev.kamilklecha.autoswitchmobile;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -17,30 +17,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Button btn;
-    private Socket socket;
-
-    private static final int SERVERPORT = 21;
-    private static final String SERVER_IP = "192.168.1.160";
-
-    private TextView mTextViewReplyFromServer;
     private EditText mEditTextSendMessage;
+    private SocketCommunicator sc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        sc = new SocketCommunicator((TextView) findViewById(R.id.tv_reply_from_server));
+
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -67,15 +57,24 @@ public class MainActivity extends AppCompatActivity
         };
         btn.setOnClickListener(cl);
 
-        Button buttonSend = (Button) findViewById(R.id.btn_send);
+        final Button buttonSend = (Button) findViewById(R.id.btn_send);
+        Button buttonSetup = (Button) findViewById(R.id.btn_setup);
 
         mEditTextSendMessage = (EditText) findViewById(R.id.edt_send_message);
-        mTextViewReplyFromServer = (TextView) findViewById(R.id.tv_reply_from_server);
 
         buttonSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMessage(mEditTextSendMessage.getText().toString());
+                sc.sendMessage(mEditTextSendMessage.getText().toString());
+            }
+        });
+
+        buttonSetup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sc.sendMessage("Hi, " + Build.MODEL + " here");
+                sc.startReceiver();
+                buttonSend.setEnabled(true);
             }
         });
     }
@@ -109,48 +108,5 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    private void sendMessage(final String msg) {
-
-        final Handler handler = new Handler();
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    //Replace below IP with the IP of that device in which server socket open.
-                    //If you change port then change the port number in the server side code also.
-                    Socket s = new Socket(SERVER_IP, SERVERPORT);
-
-                    OutputStream out = s.getOutputStream();
-
-                    PrintWriter output = new PrintWriter(out);
-
-                    output.println(msg);
-                    output.flush();
-                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-                    final String st = input.readLine();
-
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            String s = mTextViewReplyFromServer.getText().toString();
-                            if (st.trim().length() != 0)
-                                mTextViewReplyFromServer.setText(s + "\nFrom Server : " + st);
-                        }
-                    });
-
-                    output.close();
-                    out.close();
-                    s.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        thread.start();
     }
 }
